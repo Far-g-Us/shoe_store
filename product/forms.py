4170b7f855efa28139ab0889cb886243ceb5b360
Far-g-Us <schepaksin@bk.ru>
2024-03-28 06:42:23 +0400

    add-3

--- product/forms.py
+++ product/forms.py
@@ -4,53 +4,53 @@ from product.models import Product, OuterwearClothingProduct, JeansClothingProdu
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product, CareInstructionsModel
-        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions']
+        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions', 'clothing_category']
 
 
 class OuterwearForm(forms.ModelForm):
     class Meta:
         model = OuterwearClothingProduct, CareInstructionsModel
-        fields = ['name', 'image', 'description', 'price', 'volume',  'care_instructions']
+        fields = ['name', 'image', 'description', 'price', 'volume',  'care_instructions', 'clothing_category']
 
 
 class JeansForm(forms.ModelForm):
     class Meta:
         model = JeansClothingProduct, CareInstructionsModel
-        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions']
+        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions', 'clothing_category']
                   #'lining_material', 'fashion', 'style', 'compound',
 
 
 class Tshirts_and_longsleevesForm(forms.ModelForm):
     class Meta:
         model = Tshirts_and_longsleevesClothingProduct, CareInstructionsModel
-        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions']
+        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions', 'clothing_category']
 
 
 class Hoodies_and_sweatshirtsForm(forms.ModelForm):
     class Meta:
         model = Hoodies_and_sweatshirtsClothingProduct, CareInstructionsModel
-        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions']
+        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions', 'clothing_category']
 
 
 class TrousersForm(forms.ModelForm):
     class Meta:
         model = TrousersClothingProduct, CareInstructionsModel
-        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions']
+        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions', 'clothing_category']
 
 
 class ShirtsForm(forms.ModelForm):
     class Meta:
         model = ShirtsClothingProduct, CareInstructionsModel
-        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions']
+        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions', 'clothing_category']
 
 
 class SportForm(forms.ModelForm):
     class Meta:
         model = SportClothingProduct, CareInstructionsModel
-        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions']
+        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions', 'clothing_category']
 
 
 class ShortsForm(forms.ModelForm):
     class Meta:
         model = ShortsClothingProduct, CareInstructionsModel
-        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions']
+        fields = ['name', 'image', 'description', 'price', 'volume', 'care_instructions', 'clothing_category']
--- product/models.py
+++ product/models.py
@@ -1,26 +1,35 @@
 from django.db import models
 
+
+class CareInstructionsModel(models.Model):
+    care_instructions_choices = [
+        ('hand_wash', 'Только ручная стирка'),
+        ('can_be_dried_in_a_washing_machine', 'Можно сушить в сушильной или стиральной машине'),
+        ('can_be_ironed', 'Можно гладить'),
+        ('dry_cleaning', 'Сухая чистка (химчистка)')
+    ]
+    care_instructions = models.CharField(max_length=33, choices=care_instructions_choices, default='hand_wash', verbose_name='Уход')
+
+    class Meta:
+        abstract = True
+
+
+class ClothingCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name}'
+
 class Product(models.Model):
     name = models.CharField(max_length=35, verbose_name='Название')
     image = models.ImageField(verbose_name='Фото', upload_to='content/%Y/%m', blank=True)
     description = models.TextField(verbose_name='О товаре', blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     volume = models.CharField(max_length=100, verbose_name='Кол-во', blank=True, null=True)
-    #type_clothing = models.CharField
+    clothing_category = models.ForeignKey(ClothingCategory, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name}, {self.volume}'
-
-
-class CareInstructionsModel(models.Model):
-    care_instructions_choices = [
-        ('hand_wash', 'Только ручная стирка'),
-
-    ]
-    care_instructions = models.CharField(max_length=20, choices=care_instructions_choices, default='hand_wash', verbose_name='Уход')
-
-    class Meta:
-        abstract = True
 
 
 class OuterwearClothingProduct(Product, CareInstructionsModel):
